37a74ab8a9b3da5c9cea9a8a6e71c1800bd390b3
shahruj <57801700+shahruj@users.noreply.github.com>
2021-03-12 02:28:04 +0800

    Update prep_data_set_2.py

--- prep_data_set_2.py
+++ prep_data_set_2.py
@@ -18,30 +18,32 @@ class prep_data:
         dict = {"fly_ash":2,"age":7,"coarse_aggregate":5,"cement":0,"water":3,"fine_aggregate":6,"blast_furnace_slag":1,"compressive_strength":8,"superplasticizer":4}
         #Name of the s3 bucket which contains the dataset
         #Path to the s3 bucket location which contains the dataset
-        data_location = 's3://{}/data/{}'.format(self.bucketname, self.dataset) 
+        data_location = 's3://{}/cement_dataset/Version/{}'.format(self.bucketname, self.dataset) 
         df = pd.read_csv(data_location)
-        print(df)
+        #print(df)
         concrete_data = df.copy()
         for i in dict:
             concrete_data.iloc[:,dict[i]]=df[i]
             concrete_data
-        list = ["cement","blast_furnace_slag","fly_ash","water","superplasticizer","coarse_aggregate","fine_aggregate","age"]
         concrete_data.drop(concrete_data.columns[9], axis=1, inplace=True)
-        concrete_data.columns = list
-        print(concrete_data)
+        col_name = ["cement","blast_furnace_slag","fly_ash","water","superplasticizer","coarse_aggregate","fine_aggregate","age","compressive_strength"]
+        concrete_data.columns = col_name
+        #print(concrete_data)
         #concrete_data = pd.read_csv('cement.csv')
         min_d = concrete_data.min()
         max_d = concrete_data.max()
         a = []
         for i in range(0,min_d.shape[0]):
             a.append([min_d[i],max_d[i]])    
+        normalized_df=(concrete_data - min_d)/(max_d - min_d)
         limit = str(a)
         text_file = open("limits.txt", "wt")
         n = text_file.write(limit)
         text_file.close()
-        normalized_df=(concrete_data - min_d)/(max_d - min_d)
         normal_train = normalized_df.iloc[:,:8]
         normal_label = normalized_df.iloc[:,-1:]
+        print(normal_train)
+        print(normal_label)
         #Spliting the dataset
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(normal_train, normal_label, test_size = splitRatio, random_state = 1)
         self.x_train.to_csv("x_train.csv")
@@ -60,5 +62,3 @@ class prep_data:
         text_file.close()
         s3.meta.client.upload_file("model_info.txt", self.bucketname,'models/{}/{}'.format(self.modelname,'model_info.txt'))
         print("uploaded")
-
-        
